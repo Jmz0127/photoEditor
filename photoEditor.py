2d873ad478809a09d49d3a769669a6c1b6d2c730
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2023-08-09 16:00:39 -0400

    added additional brightness to edited photo

--- photoEditor.py
+++ photoEditor.py
@@ -9,6 +9,10 @@ for filename in os.listdir(path):
 
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
 
+    factor = 1.25
+    enhancer = ImageEnhance.Brightness(edit)
+    edit = enhancer.enhance(factor)
+
     clean_name = os.path.splitext(filename)[0]
     
     edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
